469c76bd924603adfd9040311d4835b996856c75
Azizul Hakim Eshan <eshazizul@gmail.com>
2021-07-20 09:16:59 +0600

    added alternate approach

--- Form2.cs
+++ Form2.cs
@@ -1,5 +1,4 @@
-﻿using Blackjacker___Simple_Card_Counting_Solution;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,36 +8,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Blackjacker___A_Card_Counting_Solution
+namespace Blackjacker___Simple_Card_Counting_Solution
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "0";
-        }
-
         private void button1_Click(object sender, EventArgs e)
-        {
-            int num = Convert.ToInt32(textBox1.Text) + 1;
-            textBox1.Text = num.ToString();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
         {
             int num = Convert.ToInt32(textBox1.Text) - 1;
             textBox1.Text = num.ToString();
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            Form2 f = new Form2();
-            f.Show();
+            int num = Convert.ToInt32(textBox1.Text) + 1;
+            textBox1.Text = num.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
         }
     }
 }
